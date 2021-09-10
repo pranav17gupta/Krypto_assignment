a0f70bd1507b6dd7b263fa7f3766496097532d7a
pranav17gupta <79404399+pranav17gupta@users.noreply.github.com>
2021-09-10 17:20:52 +0530

    Create create_alert.py

---
+++ create_alert.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow 
+from flask_restful import Resource, Api
+
+app = Flask(__name__) 
+api = Api(app) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///Alerts.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app) 
+ma = Marshmallow(app)
+
+/*Inserting the alert*/
+@staticmethod
+def post():
+    alertid = request.json['alertid']
+    userif = request.json['userid']
+    alertprice = request.json['alertprice']
+    
+    alert = Alerts(alertid, userid, alertprice)
+    db.session.add(alert)
+    db.session.commit()
+
+    return jsonify({
+        'Message': f'User {alertid} {userid} inserted.'
+    })
+
