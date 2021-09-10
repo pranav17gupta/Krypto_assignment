fb5cd42b837c3dce8b0a325247c906f98b6e4b57
pranav17gupta <79404399+pranav17gupta@users.noreply.github.com>
2021-09-10 18:22:45 +0530

    Update fetch_alerts.py

--- fetch_alerts.py
+++ fetch_alerts.py
@@ -1,3 +1,37 @@
+from flask import Flask, request, jsonify 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow 
+from flask_restful import Resource, Api
 
+app = Flask(__name__) 
+api = Api(app) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///Alerts.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app) 
+ma = Marshmallow(app)
 
+/*fetching all the alerts with status*/
+@staticmethod
+def fetchAllAlerts():
+    try: id = request.args['alertid']
+    except Exception as _: id = None
 
+    if not id:
+        alert = User.query.all()
+        return jsonify(users_schema.dump(alerts))
+    alert = Alerts.query.get(id)
+    return jsonify(user_schema.dump(alert))
+  
+/*fetching the alerts having the given status*/
+@staticmethod
+  def fetchAlertsByStatus():
+    try: status = request.args['status']
+    except Exception as _: status = None
+
+    if not id:
+        alert = User.query.all()
+        return jsonify(users_schema.dump(alerts))
+    alert = Alerts.query.get(status)
+    return jsonify(user_schema.dump(alert))
+  
+  
