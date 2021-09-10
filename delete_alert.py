a0ae278dccd2e2b295c46b49ea8d6e9cb9c3e35e
pranav17gupta <79404399+pranav17gupta@users.noreply.github.com>
2021-09-10 18:23:44 +0530

    Update delete_alert.py

--- delete_alert.py
+++ delete_alert.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__) 
 api = Api(app) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///Alerts.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app) 
 ma = Marshmallow(app)
